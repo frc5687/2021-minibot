0b90a0a4d7588423e600e3aec6ac63203a425e3a
Ben Bernard <benbernard@cliffislandsoftware.com>
2021-02-27 17:16:07 -0500

    Added gyro code to ArcadeDrive

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -6,6 +6,8 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.utils.Utilities;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import java.util.function.Supplier;
 
@@ -13,6 +15,15 @@ public class ArcadeDrive extends CommandBase {
   private final Drivetrain m_drivetrain;
   private final Supplier<Double> m_xaxisSpeedSupplier;
   private final Supplier<Double> m_zaxisRotateSupplier;
+
+  private final PIDController m_pidController;
+
+  private final double kP = 0.085;
+  private final double kI = 0.001;
+  private final double kD = 0.00;
+
+  private boolean wasStraight = false;
+
 
   /**
    * Creates a new ArcadeDrive. This command will drive your robot according to the speed supplier
@@ -29,12 +40,30 @@ public class ArcadeDrive extends CommandBase {
     m_drivetrain = drivetrain;
     m_xaxisSpeedSupplier = xaxisSpeedSupplier;
     m_zaxisRotateSupplier = zaxisRotateSuppplier;
+
+    SmartDashboard.putNumber("ArcadeDrive/p", kP);
+    SmartDashboard.putNumber("ArcadeDrive/i", kI);
+    SmartDashboard.putNumber("ArcadeDrive/d", kD);
+    m_pidController = new PIDController(kP, kI, kD);
+    m_pidController.enableContinuousInput(-180, 180);
+    m_pidController.setTolerance(0.5);
+
     addRequirements(drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_pidController.setPID(
+      SmartDashboard.getNumber("ArcadeDrive/p", kP), 
+      SmartDashboard.getNumber("ArcadeDrive/i", kI), 
+      SmartDashboard.getNumber("ArcadeDrive/d", kD)
+    );
+
+    m_pidController.reset();
+    m_drivetrain.resetGyro();
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -43,6 +72,21 @@ public class ArcadeDrive extends CommandBase {
     double rotation = m_zaxisRotateSupplier.get();
     rotation = Utilities.applyDeadband(rotation, 0.01);
     rotation = Utilities.applySensitivityFactor(rotation, 0.5);
+
+    if (Math.abs(rotation) < 0.01) {
+      if (wasStraight) {
+        double yaw = m_drivetrain.getGyroAngleZ();
+        SmartDashboard.putNumber("ArcadeDrive/yaw", yaw/180);
+        rotation = m_pidController.calculate(yaw);
+        SmartDashboard.putNumber("ArcadeDrive/correction", rotation);    
+      } else {
+        m_pidController.setSetpoint(m_drivetrain.getGyroAngleZ());
+        wasStraight = true;
+      }
+    } else {
+      wasStraight = false;
+    }
+
     m_drivetrain.arcadeDrive(speed, rotation);
   }
 
